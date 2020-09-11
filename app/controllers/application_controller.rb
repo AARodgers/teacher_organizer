b67ef2f5af0ddbf2ef82dda1a8c3d7ef1f435ca2
github username <github email address>
2020-09-11 08:36:30 -0600

    adds helper methods to application controller class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,32 +5,30 @@ class ApplicationController < Sinatra::Base
         set :views, 'app/views'
         # set :sessions_secret, "secret"
     end
-
-    # add code below
-    # helpers do
-
-    #     def logged_in?
-    #         !!current_user
-    #     end
-    # end
-
-    # def current_user
-    #     @current_user ||= User.find_by(:email => session[:email]) if session[:email]
-    # end
-
-    # def login(email, password)
-    #     user = User.find_by(:email => email)
-    #         if user && user.authenticate(password)
-    #             session[:email] = user.email
-    #         else
-    #             redirect '/login'
-    #         end
-    # end
-    # # if it can't find a user with that email, the variable user will be nil and it will redirect
-
-    # def logout!
-    #     session.clear
-    # end
+            #?? what is public folder and does it need to be set to public and is session secret already done in config.ru
 
 
+    helpers do
+
+        def logged_in?
+            !!current_user
+        end
+
+
+        def current_user
+            @current_user ||= User.find_by(:email => session[:email]) if session[:email]
+        end
+
+        def login(email, password)
+            user = User.find_by(:email => email)
+            if user && user.authenticate(password)
+                sessions[:email] = user.email
+            else
+                redirect '/login'
+            end
+        end
+
+        def logout!
+            session.clear
+        end
 end
