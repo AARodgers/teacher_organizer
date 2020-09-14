cd586100d10debcc7ab03888f3bf2629f1bda542
github username <github email address>
2020-09-13 20:43:24 -0600

    adds standards to homepage

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -2,6 +2,7 @@ class StandardsController < ApplicationController
 
     get '/homepage' do
         @user = current_user
+        @standards = @user.standards
         erb :'users/homepage'
     end
 end
