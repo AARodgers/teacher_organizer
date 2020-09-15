b76dbc2a6817d973df648c894483922e2f79dc6b
github username <github email address>
2020-09-15 17:19:25 -0600

    seeds database with new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,28 @@ resource1 = Resource.create(
     standards: [standard1]
 )
 
+user4 = User.create(name: "Teacher Tonya", email: "tonya@internet_high_school.com", password: "teach")
+
+standard2 = Standard.create(
+    title: "Various forms of electromagnetic radiation (x-rays, ultraviolet, alpha particles, beta particles, gamma particles, infrared, etc.) (SC09-GR.HS-S.1-GLE.2;RA.4;IQ.2)",
+    description: "Students should be able to identify and define the different kinds of electromagnetic radiation.",
+    users: [user4]
+)
+
+resource2 = Resource.create(
+    title: "Video summary of electromagnetic spectrum and different waves",
+    link: "https://www.youtube.com/watch?v=cfXzwh3KadE",
+    standards: [standard2]
+)
+
+resource3 = Resource.create(
+    title: "Video for Ultra-violet rays and radiation",
+    link: "https://www.youtube.com/watch?v=QW5zeVy8aE0",
+    standards: [standard2]
+)
+
+resource4 = Resource.create(
+    title: "Alpha, Beta, Gamma Decay Article to read",
+    link: "https://www.khanacademy.org/science/in-in-class-12th-physics-india/nuclei/in-in-nuclear-physics/a/radioactive-decay-types-article",
+    standards: [standard2]
+)
