637ce93d285831f2dde9b5c591b1733f9983f5be
github username <github email address>
2020-09-17 12:31:26 -0600

    adds methods to standards_controller

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -1,23 +1,34 @@
 class StandardsController < ApplicationController
 
     get '/homepage' do
-        @user = current_user
-        @standards = @user.standards
-        erb :'users/homepage'
+      @user = current_user
+      @standards = @user.standards
+      erb :'users/homepage'
     end
 
-    get '/standard/new' do
-        if logged_in?
-            erb :'standards/create_standard'
+    get '/standards/new' do
+      if logged_in?
+          erb :'standards/create_standard'
+      else
+          redirect to '/homepage'
+      end
+    end
+
+    post '/homepage' do
+      if logged_in?
+        if params[:content] == ""
+          redirect to '/standards/new'
         else
-            redirect to '/homepage'
+          @standard = current_user.standards.build(content: params[:content])
+          if @standard.save
+            redirect to "/standards/#{@standard.id}"
+          else
+            redirect to '/standards/new'
+          end
         end
+      else
+        redirect to '/homepage'
+      end
     end
-
-
-
-
-
-
 
 end
