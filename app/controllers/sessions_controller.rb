1898ec5bd523fd8eed606eadcf57d6d3827e64f7
Amanda Rodgers <aarodgerswork@gmail.com>
2020-12-12 22:01:40 -0700

    adds flash msg to login and user_homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,19 @@
 class SessionsController < ApplicationController
   # Sends message to user that they have successully signed in.
   get '/login' do
-    @successful_signup = session[:successful_signup]
-    session[:successful_signup] = nil
     erb :'sessions/login'
   end
 
   # Uses user's input to log them in and save their info.
   post '/sessions' do
-    login(params[:email], params[:password])
-    @successful_login = session[:successful_login]
-    redirect '/users/homepage'
+    if params[:email] == '' && params[:password] == ''
+      flash "Please provide your email and password!"
+      erb :'sessions/login'
+    else
+      login(params[:email], params[:password])
+      flash "You have successfully logged in!"
+      redirect "/users/homepage"
+    end
   end
 
   # Logs the user out and redirects them to main homepage.
